ca43966413604a9557cf69376467b39c8ac06ed5
GiamT <giammarco.marinelli@gmail.com>
2023-09-01 09:31:58 +0200

    Inizializzata cartella main del progetto con git

---
+++ src/test/java/com/example/serviceregistrydemo/ServiceRegistryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
